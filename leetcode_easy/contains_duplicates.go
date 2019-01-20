385165d8fbb744aa495914b953771af6ab04072c
t73liu <t73liu@gmail.com>
2019-01-20 12:52:21 -0800

    Contains Duplicate solution

---
+++ leetcode_easy/contains_duplicates.go
@@ -0,0 +1,14 @@
+package leetcode_easy
+
+func containsDuplicate(nums []int) bool {
+	exists := make(map[int]bool)
+	for i := 0; i < len(nums); i++ {
+		v, ok := exists[nums[i]]
+		if ok {
+			return v
+		} else {
+			exists[nums[i]] = true
+		}
+	}
+	return false
+}
