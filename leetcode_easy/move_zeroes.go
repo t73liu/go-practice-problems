8fa001cfac4d60dc376f946a1870e14eda69904d
t73liu <t73liu@gmail.com>
2019-01-29 22:47:02 -0800

    Move Zeroes solution

---
+++ leetcode_easy/move_zeroes.go
@@ -0,0 +1,23 @@
+package leetcode_easy
+
+func moveZeroes(nums []int) {
+	if len(nums) < 2 {
+		return
+	}
+	for zeroIndex := 0; zeroIndex < len(nums); zeroIndex++ {
+		if nums[zeroIndex] == 0 {
+			nonZeroIndex := zeroIndex + 1
+			for ; nonZeroIndex < len(nums); nonZeroIndex++ {
+				if nums[nonZeroIndex] != 0 {
+					break
+				}
+			}
+			if nonZeroIndex >= len(nums) {
+				break
+			} else {
+				nums[zeroIndex] = nums[nonZeroIndex]
+				nums[nonZeroIndex] = 0
+			}
+		}
+	}
+}
