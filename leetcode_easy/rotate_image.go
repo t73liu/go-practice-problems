2bbffd2ce5a698e9fa340b6f4f10e50a534447d3
t73liu <t73liu@gmail.com>
2019-01-30 00:40:50 -0800

    Rotate Image solution

---
+++ leetcode_easy/rotate_image.go
@@ -0,0 +1,28 @@
+package leetcode_easy
+
+func rotate_matrix(matrix [][]int) {
+	flip(matrix)
+	transpose(matrix)
+}
+
+func flip(matrix [][]int) {
+	length := len(matrix)
+	for i := 0; i < length/2; i++ {
+		for j := 0; j < length; j++ {
+			temp := matrix[i][j]
+			matrix[i][j] = matrix[length-1-i][j]
+			matrix[length-1-i][j] = temp
+		}
+	}
+}
+
+func transpose(matrix [][]int) {
+	length := len(matrix)
+	for i := 0; i < length; i++ {
+		for j := i + 1; j < length; j++ {
+			temp := matrix[i][j]
+			matrix[i][j] = matrix[j][i]
+			matrix[j][i] = temp
+		}
+	}
+}
