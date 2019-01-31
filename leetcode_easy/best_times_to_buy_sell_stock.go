6f07726c3a5693a2eea7318de4e4d5a4b7d90415
t73liu <t73liu@gmail.com>
2019-01-31 00:42:18 -0800

    Best time to buy and sell stock solution

---
+++ leetcode_easy/best_times_to_buy_sell_stock.go
@@ -0,0 +1,22 @@
+package leetcode_easy
+
+func maxProfit(prices []int) int {
+	size := len(prices)
+
+	if size < 2 {
+		return 0
+	}
+
+	profit := 0
+	low := prices[0]
+	for i := 1; i < size; i++ {
+		if prices[i] < low {
+			low = prices[i]
+		} else {
+			profit += prices[i] - low
+			low = prices[i]
+		}
+	}
+
+	return profit
+}
