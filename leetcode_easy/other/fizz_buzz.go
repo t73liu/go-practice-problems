b2787d78d0ff20c1a56f4dd6b7aaaacc419ca61d
t73liu <t73liu@gmail.com>
2019-02-26 21:11:58 -0800

    Fizz Buzz solution

---
+++ leetcode_easy/other/fizz_buzz.go
@@ -0,0 +1,21 @@
+package other
+
+import "strconv"
+
+func fizzBuzz(n int) []string {
+	arr := make([]string, n)
+	for i := 1; i <= n; i++ {
+		mod3 := i%3 == 0
+		mod5 := i%5 == 0
+		if mod3 && mod5 {
+			arr[i-1] = "FizzBuzz"
+		} else if mod3 {
+			arr[i-1] = "Fizz"
+		} else if mod5 {
+			arr[i-1] = "Buzz"
+		} else {
+			arr[i-1] = strconv.Itoa(i)
+		}
+	}
+	return arr
+}
